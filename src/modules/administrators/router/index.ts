aacea413b21c191a5ddbcfb9cc3271598bbb2038
h8a <herii8a@gmail.com>
2023-04-15 01:29:11 -0700

    [ADD] base structure users

--- src/modules/administrators/router/index.ts
+++ src/modules/administrators/router/index.ts
@@ -2,6 +2,8 @@ import type { RouteRecordRaw } from 'vue-router'
 import PostsLayout from '@/modules/administrators/posts/layouts/PostsLayout.vue'
 import PostCreateView from '@/modules/administrators/posts/views/PostCreateView.vue'
 import AuthView from '@/modules/administrators/auth/views/AuthView.vue'
+import UsersView from '@/modules/administrators/users/views/UsersView.vue'
+import UserView from '@/modules/administrators/users/views/UserView.vue'
 
 const administrationRoute: RouteRecordRaw = {
     path: '/admin',
@@ -11,12 +13,25 @@ const administrationRoute: RouteRecordRaw = {
         {
             path: '/admin/posts',
             name: 'posts',
-            component: PostCreateView
+            component: PostCreateView,
+            meta: { requireAuth: true }
         },
         {
             path: '/admin/auth',
             name: 'auth',
             component: AuthView
+        },
+        {
+            path: '/admin/users',
+            name: 'users',
+            component: UsersView,
+            meta: { requireAuth: true }
+        },
+        {
+            path: '/admin/user/:id',
+            name: 'user',
+            component: UserView,
+            meta: { requireAuth: true }
         }
     ]
 }
